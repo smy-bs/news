0e80f995bd8560f2372e78dc37c0e2f3d9a663d6
Miyoung SB <bs.miyoung@gmail.com>
2023-12-01 17:46:23 +0100

    update
    news api

---
+++ README.md
@@ -0,0 +1,13 @@
+# Times News website
+
+## User Story
+* L'utilisateur peut voir les actualités sur la page d'accueil.
+* Chaque actualité comporte une image, un titre, un contenu, une source et une date.
+* L'utilisateur peut rechercher des actualités par catégorie, telles que le sport, la technologie, les affaires, etc.
+* L'utilisateur peut rechercher les actualités par mot-clé.
+* Le site web est adaptatif, offrant une expérience sur des pages web réactives.
+* L'utilisateur peut utiliser la pagination pour obtenir plus d'actualités.
+
+## API Docuemnt
+* API credit to :https://newsapi.org/docs
+
--- main.js
+++ main.js
@@ -1,55 +1,168 @@
-const API_KEY = `63f5f229f63e4668897bf96f197f7b39`;
-let newsList = [];
-const menus = document.querySelectorAll(".menus button");
- menus.forEach(menu =>
- menu.addEventListener("click", (event) => 
- getNewsByCategory(event))
- );
+const API_KEY = "63f5f229f63e4668897bf96f197f7b39";
+let articles = [];
+let page = 1;
+let totalPage = 1;
+const PAGE_SIZE = 10;
+let url = new URL(
+  `https://newsapi.org/v2/top-headlines?country=us&pageSize=${PAGE_SIZE}`
+);
+let menus = document.querySelectorAll("#menu-list button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (e) => getNewsByTopic(e))
+);
 
-const getLatestNews = async () => {
-  const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+const getNews = async () => {
+  try {
+    url.searchParams.set("page", page);
+    let response = await fetch(url);
+    let data = await response.json();
+    if (response.status == 200) {
+      if (data.totalResults == 0) {
+        page = 0;
+        totalPage = 0;
+        renderPagination();
+        throw new Error("Aucun résultat ne correspond à la recherche.");
+      }
+
+      articles = data.articles;
+      totalPage = Math.ceil(data.totalResults / PAGE_SIZE);
+      render();
+      renderPagination();
+    } else {
+      page = 0;
+      totalPage = 0;
+      renderPagination();
+      throw new Error(data.message);
+    }
+  } catch (e) {
+    errorRender(e.message);
+    page = 0;
+    totalPage = 0;
+    renderPagination();
+  }
+};
+const getLatestNews = () => {
+  page = 1; 
+  url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=${PAGE_SIZE}&apiKey=${API_KEY}`
   );
 
-  const response = await fetch(url);
-  const data = await response.json();
-  newsList = data.articles;
-  render();
- };
+  getNews();
+};
 
-const getNewsByCategory = async (event)=>{
-    const category = event.target.textContent.toLowerCase();
-     console.log("category", category);
-     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
-         );
-       const response = await fetch(url);
-      const date = await response.json();
-             newsList = data.articles;
-         render();
-       
+const getNewsByTopic = (event) => {
+  const topic = event.target.textContent.toLowerCase();
+
+  page = 1;
+  url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=${PAGE_SIZE}&category=${topic}&apiKey=${API_KEY}`
+  );
+
+  getNews();
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
+const getNewsByKeyword = () => {
+  const keyword = document.getElementById("search-input").value;
+
+  page = 1;
+  url = new URL(
+    `https://newsapi.org/v2/top-headlines?q=${keyword}&country=us&pageSize=${PAGE_SIZE}&apiKey=${API_KEY}`
+  );
+
+  getNews();
 };
 
 const render = () => {
-  const newsHTML = newsList.map(
-    (news) => `<div class="row news">
-     <div class="col-lg-4">
-       <img
-         class="new-img-size"
-         src=${news.urlToImage}
-         alt=""
-       />
-     </div>
-     <div class="col-lg-8">
-       <h2>${news.title}</h2>
-       <p>
-      ${news.description}
-       </p>
-     </div>
-     <div>${news.source.name}*${news.publishedAt}</div>
-   </div>`
-  ).join('');
-  document.getElementById("news-board").innerHTML = newsHTML;
+  let resultHTML = articles
+    .map((news) => {
+      return `<div class="news row">
+        <div class="col-lg-4">
+            <img class="news-img"
+                src="${
+                  news.urlToImage ||
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+                }" />
+        </div>
+        <div class="col-lg-8">
+            <a class="title" target="_blank" href="${news.url}">${
+        news.title
+      }</a>
+            <p>${
+              news.description == null || news.description == ""
+                ? "Pas de contenu"
+                : news.description.length > 200
+                ? news.description.substring(0, 200) + "..."
+                : news.description
+            }</p>
+            <div>${news.source.name || "no source"}  ${moment(
+        news.publishedAt
+      ).fromNow()}</div>
+        </div>
+    </div>`;
+    })
+    .join("");
+
+  document.getElementById("news-board").innerHTML = resultHTML;
+};
+const renderPagination = () => {
+  let paginationHTML = ``;
+  let pageGroup = Math.ceil(page / 5);
+  let last = pageGroup * 5;
+  if (last > totalPage) {
+    last = totalPage;
+  }
+  let first = last - 4 <= 0 ? 1 : last - 4;
+  if (first >= 6) {
+    paginationHTML = `<li class="page-item" onclick="pageClick(1)">
+                        <a class="page-link" href='#js-bottom'>&lt;&lt;</a>
+                      </li>
+                      <li class="page-item" onclick="pageClick(${page - 1})">
+                        <a class="page-link" href='#js-bottom'>&lt;</a>
+                      </li>`;
+  }
+  for (let i = first; i <= last; i++) {
+    paginationHTML += `<li class="page-item ${i == page ? "active" : ""}" >
+                        <a class="page-link" href='#js-bottom' onclick="pageClick(${i})" >${i}</a>
+                       </li>`;
+  }
+
+  if (last < totalPage) {
+    paginationHTML += `<li class="page-item" onclick="pageClick(${page + 1})">
+                        <a  class="page-link" href='#js-program-detail-bottom'>&gt;</a>
+                       </li>
+                       <li class="page-item" onclick="pageClick(${totalPage})">
+                        <a class="page-link" href='#js-bottom'>&gt;&gt;</a>
+                       </li>`;
+  }
+
+  document.querySelector(".pagination").innerHTML = paginationHTML;
 };
 
+const pageClick = (pageNum) => {
+  page = pageNum;
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  getNews();
+};
+const errorRender = (message) => {
+  document.getElementById(
+    "news-board"
+  ).innerHTML = `<h3 class="text-center alert alert-danger mt-1">${message}</h3>`;
+};
+
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
 getLatestNews();
